321877eda6cd0983b97d42743a1e64e57fa32f02
migueljusto-2307 <justomiguel2307@gmail.com>
2026-09-09 17:01:35 -0300

    atualizacao

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,17 @@
-fetch('http://localhost:3000/api/mensagem')
-    .then(response => response.json())
-    .then(data => {
+async function dataConsumo() {
+    try {
+        const resposta = await fetch('http://localhost:3000/api/mensagem')
+        if (!resposta.ok){
+            throw new Error (`Erro HTTP: ${resposta.status}`); 
+        }
+        const data = await resposta.json();
         const msn = document.getElementById('texto');
-        const mns = document.getElementById('text');
         msn.innerHTML = (data.mensagem)
+        const mns = document.getElementById('text');
         mns.innerHTML = (data.sub)
-    });
+    }
+    catch (erro) {
+        console.error('Erro:', erro)
+    }
+}
+dataConsumo()
