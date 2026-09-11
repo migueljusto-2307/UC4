cbea681f915c48c12e92d0f5ca252c47fb5b7049
migueljusto-2307 <justomiguel2307@gmail.com>
2026-09-11 17:01:11 -0300

    melhorias script

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,50 @@
-const express=require('express');
-const cors=require('cors');
+const express=require('express');//iniciamos o express
+const cors=require('cors');//iniciamos o roteamento do cors
+const fs = require('fs');//inserimos edição de arquivos
 
-const app=express();
-const PORT=3000;
+const app=express();//definimos o app para utilizar o express()
 
-app.use(cors());
-app.use(express.json());
+const PORT=3000;//definimos a porta do back
+const ARQUIVO = "./dados.json"; //definimos o arquivo que os dados serão persistidos
 
-app.get('/api/mensagem' , (req, res) => {
-    res.json({mensagem: 'Olá do backend com Express!' , sub:'Meu segundo elemento'});
+app.use(cors());//ativamos o app para usar o cors
+app.use(express.json());//ativamos o app para utilizar estruturas json
+
+//funcoes auxiliares 
+function leituraUsuarios(){
+    const dados = fs.readFileSync(ARQUIVO , "utf-8")
+    return JSON.parse(dados);
+}
+
+function salvarUsuarios(usuarios){
+    fs.writeFilesSync(ARQUIVO, JSON.stringify(usuarios, null , 2));
+}
+
+
+//Get: listar todos
+app.get('/api/usuarios' , (req, res) => {
+    const usuarios = leituraUsuarios();
+    res.json(usuarios);
 });
 
+//POST: criar
+app.post('api/usuarios' , (req, res) => {
+    const {nome, email} = req.body;//leitura do req do body
+
+    if(!nome || !email) { //valida campo para nao vir vazio
+        return res.status(400).json({mensagem: "Nome e email são obrigatórios"});
+    }
+
+    const usuarios = leituraUsuarios();
+
+    const novoUsuario = {id: Date.now(), nome, email};//fazemos os objetivo do novo usario
+ 
+    usuarios.push(novoUsuario);//adicionamos ao final da lista de usuarios
+
+    salvarUsuarios(usuarios);//salvamos o usuario no arquivo
+
+    res.status(201).json(novoUsuario);//retorna sucesso ao criar novo usuario
+})   
 app.listen(PORT, ( ) => {
     console.log(`Servidor atualizado em http://localhost:${PORT}`);
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,34 @@
-async function dataConsumo() {
+const API_URL = 'http://localhost:3000/api/usuarios'
+const listaUsuarios = document.getElementById('listaUsuarios')
+
+const form = document.getElementById("formUsuario")
+const nomeImput = document.getElementById("nome")
+const emailImput = document.getElementById("email")
+const botaoImput = document.getElementById("botaoSalvar")
+
+async function carregarUsuarios() {
     try {
-        const resposta = await fetch('http://localhost:3000/api/mensagem')
+        const resposta = await fetch(API_URL)
         if (!resposta.ok){
             throw new Error (`Erro HTTP: ${resposta.status}`); 
         }
         const data = await resposta.json();
-        const msn = document.getElementById('texto');
-        msn.innerHTML = (data.mensagem)
-        const mns = document.getElementById('text');
-        mns.innerHTML = (data.sub)
-    }
+        listaUsuarios.innerHTML = ";"
+        
+        data.forEach(usuario =>{
+            const linha = document.createElement("tr")
+
+            linha.innerHTML`<td>${usuario.id}</td><td>${usuario.nome}</td><td>${usuario.email}</td>`;
+
+            listaUsuarios.appendChild(linha);
+        })
+    
+    
+        }
     catch (erro) {
-        console.error('Erro:', erro)
+        console.error('Erro:', erro);
     }
 }
-dataConsumo()
+
+//inicia já com nossa listagem
+carregarUsuarios();
