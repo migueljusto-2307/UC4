f2fe135638c7e1458064b4f3ae3f82198694ca18
migueljusto-2307 <justomiguel2307@gmail.com>
2026-09-16 14:36:12 -0300

    atualizar

--- frontend/script.js
+++ frontend/script.js
@@ -7,28 +7,54 @@ const emailImput = document.getElementById("email")
 const botaoImput = document.getElementById("botaoSalvar")
 
 async function carregarUsuarios() {
-    try {
-        const resposta = await fetch(API_URL)
-        if (!resposta.ok){
-            throw new Error (`Erro HTTP: ${resposta.status}`); 
+        try {
+            const resposta = await fetch(API_URL);
+            if (!resposta.ok){
+                throw new Error (`Erro HTTP: ${resposta.status}`); 
         }
         const data = await resposta.json();
-        listaUsuarios.innerHTML = ";"
-        
+        listaUsuarios.innerHTML = "";
+
         data.forEach(usuario =>{
-            const linha = document.createElement("tr")
-
-            linha.innerHTML`<td>${usuario.id}</td><td>${usuario.nome}</td><td>${usuario.email}</td>`;
-
-            listaUsuarios.appendChild(linha);
+                const linha = document.createElement("tr")
+                
+                linha.innerHTML = `<td>${usuario.id}</td><td>${usuario.nome}</td><td>${usuario.email}</td>`;
+                
+                listaUsuarios.appendChild(linha);
         })
-    
-    
+
+
         }
     catch (erro) {
-        console.error('Erro:', erro);
+            console.error('Erro:', erro);
+
     }
-}
+};
+
+form.addEventListener("submit", async (evento) => {//inicia a leitura do botao
+    evento.preventDefault();    //nao deixa a pagina atualizar
+
+    const nome = nomeImput.value;
+    const email = emailImput.value;
+
+    const usuario = {
+        nome,
+        email
+    };
+
+    await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuario)
+    });
+    carregarUsuarios();
+});
+
+
+
+
 
 //inicia já com nossa listagem
 carregarUsuarios();
