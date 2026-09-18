bfaf4ad841de976a2a0c55007b7b39c72b143926
migueljusto-2307 <justomiguel2307@gmail.com>
2026-09-18 15:06:45 -0300

    attt

--- backend/server.js
+++ backend/server.js
@@ -17,32 +17,32 @@ function leituraUsuarios(){
 }
 
 function salvarUsuarios(usuarios){
-    fs.writeFileSync(ARQUIVO, JSON.stringify(usuarios, null , 2));
+    fs.writeFileSync(ARQUIVO, JSON.stringify(usuarios, null, 2));
 }
 
 
 //Get: listar todos
-app.get('/api/usuarios' , (req, res) => {
+app.get('/api/usuarios', (req, res) => {
     const usuarios = leituraUsuarios();
     res.json(usuarios);
 });
 
 
 
-app.get('/api/usuarios/:id' , (req, res) => {
+app.get('/api/usuarios/:id', (req, res) => {
     const usuarios = leituraUsuarios();
     const id = Number(req.params.id);
     const usuario = usuarios.find(usuario => usuario.id === id);
     if (!usuario){
         return res.status(404).json({
             mensagem: "Usuário não encontrado"
-        })
+        });
     }
     res.json(usuario);
 });
 
 //POST: criar
-app.post('/api/usuarios' , (req, res) => {
+app.post('/api/usuarios', (req, res) => {
     const {nome, email} = req.body;//leitura do req do body
 
     if(!nome || !email) { //valida campo para nao vir vazio
@@ -59,21 +59,21 @@ app.post('/api/usuarios' , (req, res) => {
     salvarUsuarios(usuarios);//salvamos o usuario no arquivo
 
     res.status(201).json(novoUsuario);//retorna sucesso ao criar novo usuario
-}) 
+});
 
 //PUT: editar
 
 app.put('/api/usuarios/:id', (req, res) => {
-    const {nome,email} = req.body;// pega a infromacao do corpo da requisicao
+    const {nome, email} = req.body;// pega a infromacao do corpo da requisicao
     const usuarios = leituraUsuarios();//funcao de leitura 
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
 
-    const usuario = usuarios.find(usuario => usuario.id === id)
+    const usuario = usuarios.find(usuario => usuario.id === id);
     //find vai procurar na funcao se usuario id e igual em tipo e valor do id passado pelo front
     usuario.nome = nome;
     usuario.email = email;
     
-    salvarUsuarios();
+    salvarUsuarios(usuarios);
 
     res.json(usuario);
 }
@@ -85,7 +85,7 @@ app.put('/api/usuarios/:id', (req, res) => {
 
 
 
-app.listen(PORT, ( ) => {
+app.listen(PORT, () => {
     console.log(`Servidor atualizado em http://localhost:${PORT}`);
 });
 
